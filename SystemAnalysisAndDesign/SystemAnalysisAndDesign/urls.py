4903245699fbfae8630a9b78eb75f70f62ed1a44
chenmj <304589072@qq.com>
2016-09-17 22:22:57 +0800

    add a test url

--- SystemAnalysisAndDesign/SystemAnalysisAndDesign/urls.py
+++ SystemAnalysisAndDesign/SystemAnalysisAndDesign/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,3 +16,7 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns("ResearchPlatform.views",
+	url(r'^index/$', TemplateView.as_view(template_name="test.html")),
+	)
